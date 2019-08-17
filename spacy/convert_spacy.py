84a8144334521b8f6a0cab23eecaf91dfeeadb04
Avram Andrei Marius <avramandrei9666@gmail.com>
2019-08-17 19:04:15 +0300

    Updated to spacy 2.1.8, removed eval dataset.

--- spacy/convert_spacy.py
+++ spacy/convert_spacy.py
@@ -13,14 +13,14 @@ import subprocess
 import os
 
 
-def create_file_json_collubio(sentences, output_directory, output_filename):
+def create_file_json_collubio(sentences, output_path, output_filename):
     """
         This function creates a json file that contains CoNLL-U BIO information about RONEC.
 
         Args:
             sentences (list): list containing the train, dev or eval sentences.
             output_directory (str): the output directory of the resulted files.
-            output_filename (str): the output file name: train_eorenc.json, dev_roenc.json or eval_roenc.json.
+            output_filename (str): the output file name: train_ronec.json, dev_ronec.json or eval_ronec.json.
     """
 
     # write all the selected sentences to a temporary file in CoNLL-U BIO format
@@ -30,38 +30,32 @@ def create_file_json_collubio(sentences, output_directory, output_filename):
                 if type(tokens_list) == str:
                     file.write(tokens_list)
                 else:
-                    file.write("\t".join(tokens_list)[:-1] + "\n")
+                    file.write("\t".join(tokens_list) + "\n")
 
             file.write("\n")
 
-    # the path of the Spacy json CoNLL-U BIO file
-    output_path = os.path.join(output_directory, output_filename)
-
-    # delete the desired file if it already exists
-    if os.path.exists(output_path):
-        os.remove(output_path)
-
     # convert the CoNLL-U BIO temporary file file to Spacy json CoNLL-U BIO
-    _ = subprocess.run("python -m spacy convert {} {} --converter conllubio".format(output_filename, output_directory))
+    _ = subprocess.run("python -m spacy convert {} {} --converter conllu".
+                       format(output_filename, output_path))
 
     # clean up
     os.remove(output_filename)
 
 
-def extract_sentences_from_file(roenc_path):
+def extract_sentences_from_file(ronec_path):
     """
-        This function extracts the sentences with the tokens from the ROENC and puts them in a list.
+        This function extracts the sentences with the tokens from the RONEC and puts them in a list.
 
         Args:
-            roenc_path (str): path to ROENC
+            ronec_path (str): path to RONEC
 
         Returns:
-            A list that contains the sentences of the ROENC.
+            A list that contains the sentences of the RONEC.
     """
     sentences = []
 
     # creating a temp file that can be read by spacy convertor
-    with open(roenc_path, "r", encoding="utf-8") as file:
+    with open(ronec_path, "r", encoding="utf-8") as file:
 
         old_entity = None
 
@@ -87,13 +81,13 @@ def extract_sentences_from_file(roenc_path):
                 entity = tokens[-1]
 
                 if entity == "*":
-                    tokens[-1] = "O "
+                    tokens[-1] = "O"
 
                 elif entity.__contains__(":"):
                     old_entity = entity.split(":")[-1]
-                    tokens[-1] = "I-" + old_entity
+                    tokens[-1] = old_entity
                 else:
-                    tokens[-1] = "I-" + old_entity
+                    tokens[-1] = old_entity
 
                 tokens_list.append(tokens)
 
@@ -103,39 +97,35 @@ def extract_sentences_from_file(roenc_path):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("roenc_path")
+    parser.add_argument("ronec_path")
     parser.add_argument("output_path")
 
     args = parser.parse_args()
 
-    roenc_path = args.roenc_path
+    ronec_path = args.ronec_path
     output_path = args.output_path
 
-    # extract the sentences from ROENC
-    sentences = extract_sentences_from_file(roenc_path)
+    # extract the sentences from RONEC
+    sentences = extract_sentences_from_file(ronec_path)
 
     # create the train, dev and eval sentences
     num_sentences = 5127  # source: https://github.com/dumitrescustefan/ronec
-    num_train_sentences = int(0.8 * num_sentences)
-    num_dev_sentences = int(0.1 * num_sentences)
-    num_eval_sentences = int(0.1 * num_sentences)
+    num_train_sentences = int(0.9 * num_sentences)
+    num_dev_sentences = num_sentences - num_train_sentences
 
     train_sentences = sentences[0: num_train_sentences]
     dev_sentences = sentences[num_train_sentences: num_train_sentences + num_dev_sentences]
-    eval_sentences = sentences[num_train_sentences + num_dev_sentences: num_sentences]
 
     # create the train, dev and eval json files necessary for Spacy
     print("Running convert conllup to spacy json script...\n")
-    print("Total sentences: {}\nTrain sentences: {}\nDev sentences: {}\nEval senteces: {}".
-          format(num_sentences, num_train_sentences, num_dev_sentences, num_eval_sentences))
+    print("Total sentences: {}\nTrain sentences: {}\nDev sentences: {}".
+          format(num_sentences, num_train_sentences, num_dev_sentences))
 
     create_file_json_collubio(train_sentences,
-                              output_path, "train_roenc.json")
+                              output_path, "train_ronec.json")
 
     create_file_json_collubio(dev_sentences,
-                              output_path, "dev_roenc.json")
+                              output_path, "dev_ronec.json")
 
-    create_file_json_collubio(eval_sentences,
-                              output_path, "eval_roenc.json")
 
 
